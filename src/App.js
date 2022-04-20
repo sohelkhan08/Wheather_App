b3c481513bf2260d979d473912a44b1e8a36e431
soheldavuji <soheldavuji@gmail.com>
2022-04-20 15:56:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [city,setCity] =useState('')
+  const [result,setResult] =useState('')
+  const changeHandler =e=>{
+    setCity(e.target.value)
+  }
+  const submitHandler =e=>{
+    e.preventDefault();
+    // console.log(city);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response=>response.json()
+    ).then(data=>{
+      const kelvin =data.main.temp;
+      const celcius =kelvin-273.15;
+      setResult('Temperature  at '+city+" \n "+Math.round(celcius)+'°C');
+    }
+      ).catch(error=>console.log(error))
+      setCity("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <center>
+      <h1>Wheather App</h1>
+      <form onSubmit={submitHandler}>
+        <input type="text" name='city' value={city} onChange={changeHandler}/><br /><br />
+        <input type="submit" value='Get Temperature' />
+      </form>
+      <h1>{result}</h1>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
